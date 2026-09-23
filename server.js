bc403403ca24ce1fb5b4bfbc8e24999cc178ce0e
mohammedkokkez-commits <mohammedkokkez@gmail.com>
2026-09-23 18:13:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ app.get('/chat', requireAuth, (req, res) => {
     '<input type="hidden" name="csrf" value="' + req.session.csrf + '">' +
     '<button type="submit" class="link-btn danger">مسح المحادثة بالكامل</button>' +
     '</form>'+
-'<button onclick="window.location.reload();" class="link-btn" style="margin-top: 10px;">تحديث المحادثة 🔄</button>' +
+'<button type="button" onclick="location.reload();" class="link-btn" style="margin-top: 10px;">تحديث المحادثة 🔄</button>' +
 '</form>';
 
 
