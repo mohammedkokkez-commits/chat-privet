c81e52c2e635f6f2f19ff2c218206959707eeebd
mohammedkokkez-commits <mohammedkokkez@gmail.com>
2026-09-23 17:49:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -239,7 +239,8 @@ app.get('/chat', requireAuth, (req, res) => {
 '<input type="hidden" name="csrf" value="' + req.session.csrf + '">' +
 '<button type="submit" class="link-btn danger">مسح المحادثة بالكامل</button>' +
 '</form>' +
-'<button onclick="window.location.reload();" class="link-btn">تحديث الصفحة 🔄</button>'+
+'<button onclick="window.location.reload();" class="link-btn">تحديث الصفحة 🔄</button>' +
+'</form>' +
 
 // أضف هذا السطر هنا ليظهر زر التحديث اليدوي
 '<button onclick="window.location.reload();" class="link-btn" style="margin-top: 10px;">تحديث المحادثة 🔄</button>'
